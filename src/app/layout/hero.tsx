a51f7e6eb3157b64b7c2d6180490e8a81b46e690
nadws <nandw567@gmail.com>
2024-04-30 16:46:52 +0800

    update page.tsx

---
+++ src/app/layout/hero.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export default function Hero() {
+  return (
+    <div className="flex flex-row items-center  justify-center  ">
+      <motion.div
+        className="hidden lg:block lg:w-[50%] p-8"
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeInOut", duration: 1 }}
+      >
+        <h1 className="text-6xl font-bold">
+          Jasa Bimbingan Pembuatan Skripsi, Tesis dan Disertasi
+        </h1>
+      </motion.div>
+
+      <motion.div
+        className=" w-[90%] lg:w-[50%]"
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeInOut", duration: 1.25 }}
+      >
+        <Image
+          src="/hero.jpg"
+          width="600"
+          height="600"
+          alt="Logo"
+          className="rounded-3xl lg:rotate-3 shadow-black dark:shadow-white shadow-2xl"
+        />
+        <h1 className="text-3xl font-bold mt-12 text-center lg:hidden">
+          Jasa Bimbingan Pembuatan Skripsi, Tesis dan Disertasi
+        </h1>
+      </motion.div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,13 @@
 "use client";
-import Image from "next/image";
-import { motion } from "framer-motion";
+import Hero from "./layout/hero";
+import { motion, useScroll } from "framer-motion";
 
 export default function Home() {
+  const { scrollYProgress } = useScroll();
   return (
-    <main className="flex flex-row items-center  justify-center  ">
-      <motion.div
-        className="hidden lg:block lg:w-[50%] p-8"
-        initial={{ y: 20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ ease: "easeInOut", duration: 1 }}
-      >
-        <h1 className="text-6xl font-bold">
-          Jasa Bimbingan Pembuatan Skripsi, Tesis dan Disertasi
-        </h1>
-      </motion.div>
-
-      <motion.div
-        className=" w-[90%] lg:w-[50%]"
-        initial={{ y: 20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ ease: "easeInOut", duration: 1.25 }}
-      >
-        <Image
-          src="/hero.jpg"
-          width="600"
-          height="600"
-          alt="Logo"
-          className="rounded-3xl lg:rotate-3 shadow-black dark:shadow-white shadow-2xl"
-        />
-        <h1 className="text-3xl font-bold mt-12 text-center lg:hidden">
-          Jasa Bimbingan Pembuatan Skripsi, Tesis dan Disertasi
-        </h1>
-      </motion.div>
+    <main className="h-screen">
+      <Hero />
+      <br />
     </main>
   );
 }
